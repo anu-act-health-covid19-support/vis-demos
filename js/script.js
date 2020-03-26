62e1f743501796282179da0fc550ed36381f7f23
Ben Swift <ben@benswift.me>
2020-03-26 19:53:50 +1100

    add time slider

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoia2F0aHlyZWlkIiwiYSI6ImNrODgyMTN1ZjAwamQzbW1za3Qwa2VhM20ifQ.t2zzot1Qhgqf-EzMAhIFgQ';
 
-var map = new mapboxgl.Map({
+const map = new mapboxgl.Map({
   container: 'map', // container element id
   style: 'mapbox://styles/mapbox/light-v10',
   center: [149.119, -35.280], // initial map center in [lon, lat]
@@ -20,14 +20,34 @@ const populateLegend = () => {
 
   for (i = 0; i < timeColorMapping.length; i++) {
 
-	let label = document.createElement('label');
-	let dt = new Date(timeColorMapping[i][0]);
+	const label = document.createElement('label');
+	const dt = new Date(timeColorMapping[i][0]);
 	label.style.background = timeColorMapping[i][1];
 	label.innerHTML = dt.toLocaleString();
 	legend.appendChild(label);
   }
-}
+};
 
+const configureSlider = () => {
+  const slider = document.getElementById('slider');
+  slider.min = timeColorMapping[0][0];
+  slider.value = timeColorMapping[0][0];
+  slider.max = timeColorMapping[timeColorMapping.length - 1][0];
+};
+
+const filterDotsInTimeWindow = (ts) => {
+
+  console.log(ts);
+  const windowSize = 3*60*60*1000; // 3 hours
+
+  const windowFilters = ['all',
+				 ['>', 'time', ts-windowSize/2],
+				 ['<', 'time', ts+windowSize/2]];
+  map.setFilter('track-and-trace', windowFilters);
+  // Set the label to the month
+  const dt = new Date(ts);
+  document.getElementById('time-window-centrepoint').textContent = dt.toLocaleString();
+}
 
 map.on('load', function() {
 
@@ -52,6 +72,14 @@ map.on('load', function() {
   });
 
   populateLegend();
+  configureSlider();
+
+  document
+	.getElementById('slider')
+	.addEventListener('input', function(e) {
+	  const ts = parseInt(e.target.value, 10);
+	  filterDotsInTimeWindow(ts);
+	});
 });
 
 
