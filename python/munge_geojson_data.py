b5a541a564e4c0c8a8c613cada58054d4b6c1d2a
Ben Swift <ben@benswift.me>
2020-03-26 16:51:32 +1100

    refactor: allow for adding data properties
    this is all filthy hacks, but people are dying... so I'll fix them later.

--- python/munge_geojson_data.py
+++ python/munge_geojson_data.py
@@ -2,26 +2,66 @@
 import json
 import datetime
 
-source_file = "../data/raw-data.geojson"
+raw_data_file = "../data/raw-data.geojson"
 destination_file = "../data/processed-data.geojson"
+
+
+def read_data(filename):
+
+    with open(filename) as in_f:
+        data = json.load(in_f)
+
+    print(f"read geojson data from {filename}")
+
+    return data
+
+
+def write_data(data, filename):
+
+    with open(filename, "w") as out_f:
+        json.dump(data, out_f, sort_keys=True, indent=2)
+
+    print(f"wrote geojson data to {filename}")
+
+
+def epochs_to_iso8601(data):
+    """munge timestamps in-place, from unix epoch timestamps (note, in millis!) to
+    a format which will work with `new Date()` in js
+
+    """
+    for feature in data["features"]:
+        props = feature["properties"]
+        props["time"] = datetime.datetime.fromtimestamp(
+            props["time"] / 1000 # note: timestamp is in millis
+        ).isoformat()
+
+
+def add_normalised_colour_values(data):
+    """add another prop for use with colouring
+
+    this assumes the timestamps are still in unix epoch (i.e. ints)
+
+    """
+    epochs = [feature["properties"]["time"] for feature in data["features"]]
+    minTs = min(epochs)
+    maxTs = max(epochs)
+
+    for feature in data["features"]:
+        props = feature["properties"]
+        props["colour"] = (props["time"] - minTs)/(maxTs - minTs)
 
 
 def main():
 
-    with open(source_file) as in_f:
-        data = json.load(in_f)
+    data = read_data(raw_data_file)
 
-    for feature in data["features"]:
-        props = feature["properties"]
-        props["time"] = datetime.datetime.fromtimestamp(
-            props["time"] / 1000
-        ).isoformat()  # timestamp is in millis
+    # epochs_to_iso8601(data)
 
-    with open(destination_file, "w") as out_f:
-        json.dump(data, out_f)
+    add_normalised_colour_values(data)
+
+    write_data(data, destination_file)
 
 
 if __name__ == "__main__":
     main()
 
-    print(f"wrote geojson data (with ISO8601 timestamps) to {destination_file}")
