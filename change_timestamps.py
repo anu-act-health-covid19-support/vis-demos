81ca2756c3957b0e962ba2eb4447f7f072edb74f
Ben Swift <ben@benswift.me>
2020-03-26 14:52:56 +1100

    add change_timestamps.py script

---
+++ change_timestamps.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import json
+import time
+import datetime
+
+source_file = "../geojson-over-time/rawdata.geojson"
+destination_file =  "../geojson-over-time/processed-data.geojson"
+
+def main():
+
+    with open(source_file) as in_f:
+        data = json.load(in_f)
+
+    for feature in data["features"]:
+        props = feature["properties"]
+        props["time"] = datetime.datetime.fromtimestamp(props["time"]/1000).isoformat() # timestamp is in millis
+
+    with open(destination_file, 'w') as out_f:
+        json.dump(data, out_f)
+
+if __name__ == '__main__':
+    main()
+
+    print("all done.")
