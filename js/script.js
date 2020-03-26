3058448d94a8bfa346892c4b93d5ff4d73849931
Ben Swift <ben@benswift.me>
2020-03-26 19:25:48 +1100

    the legend works
    it's so gross - I can't believe the mapbox API requires you to do this so manually...

--- js/script.js
+++ js/script.js
@@ -9,7 +9,26 @@ var map = new mapboxgl.Map({
 
 const timeColorMapping = [[1584685015006, "#eb4d4b"], [1585094785740, "#4834d4"]];
 
+// cmon mapbox, surely there's a better way to do this...
+const populateLegend = () => {
+  const legend = document.getElementById('legend');
+
+  let spans = [];
+  let labels = [];
+
+  for (i = 0; i < timeColorMapping.length; i++) {
+
+	let label = document.createElement('label');
+	let dt = new Date(timeColorMapping[i][0]);
+	label.style.background = timeColorMapping[i][1];
+	label.innerHTML = dt.toLocaleString();
+	legend.appendChild(label);
+  }
+}
+
+
 map.on('load', function() {
+
   map.addLayer({
 	id: 'track-and-trace',
 	type: 'circle',
@@ -25,8 +44,12 @@ map.on('load', function() {
 					   ['linear'],
 					   ['get', 'time'],
 					   ...(timeColorMapping.flat()), // don't get too clever, Ben...
-	  ],
+					  ],
 	  'circle-opacity': 0.8
 	}
   });
+
+  populateLegend();
 });
+
+
