107742119ca62566add38542a0736e506a094984
Ben Swift <ben@benswift.me>
2020-03-29 21:36:23 +1100

    add fancy progress bar to shouldistayathome page

---
+++ shouldistayathome/browser.js
@@ -0,0 +1,39 @@
+const ProgressBar = require("progressbar.js");
+
+// Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
+
+const analysisMSec = 5 * 1000; // 5 sec
+
+const bar = new ProgressBar.Line("#progress-bar", {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: analysisMSec,
+  color: '#FFEA82',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'},
+  text: {
+    style: {
+      color: '#999',
+      position: 'relative',
+      padding: 0,
+      margin: 0,
+      transform: null
+    },
+    autoStyleContainer: true
+  },
+  from: {color: '#22EA82'},
+  to: {color: '#ED6A5A'},
+  step: (state, bar) => {
+    bar.setText(`data analysis ${Math.round(bar.value() * 100)}% complete`);
+  }
+});
+
+bar.animate(1);  // Number from 0.0 to 1.0
+
+// based on Charles' code
+const revealAnswer = () => {
+  document.getElementById("progress-bar").remove();
+  document.getElementById("answer-section").style.visibility = "visible";
+};
+const timer = setTimeout(revealAnswer, analysisMSec + 100);
