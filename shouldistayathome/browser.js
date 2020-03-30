6c1a4eddd7b92f2fd9b8ce009c69c66fe504bf2a
Ben Swift <ben@benswift.me>
2020-03-30 15:06:15 +1100

    nicer nav links

--- shouldistayathome/browser.js
+++ shouldistayathome/browser.js
@@ -6,30 +6,32 @@ const analysisMSec = 5 * 1000; // 5 sec
 
 const bar = new ProgressBar.Line("#progress-bar", {
   strokeWidth: 4,
-  easing: 'easeInOut',
+  easing: "easeInOut",
   duration: analysisMSec,
-  color: '#118bee',
-  trailColor: '#eee',
+  color: "#118bee",
+  trailColor: "#eee",
   trailWidth: 1,
-  svgStyle: {width: '100%', height: '100%'},
+  svgStyle: { width: "100%", height: "100%" },
   text: {
     style: {
-      color: '#999',
-      position: 'relative',
+      color: "#999",
+      position: "relative",
       padding: 0,
       margin: 0,
       transform: null
     },
     autoStyleContainer: true
   },
-  from: {color: '#22EA82'},
-  to: {color: '#ED6A5A'},
+  from: { color: "#22EA82" },
+  to: { color: "#ED6A5A" },
   step: (state, bar) => {
-    bar.setText(`processing: data analysis ${Math.round(bar.value() * 100)}% complete`);
+    bar.setText(
+      `processing: data analysis ${Math.round(bar.value() * 100)}% complete`
+    );
   }
 });
 
-bar.animate(1);  // Number from 0.0 to 1.0
+bar.animate(1); // Number from 0.0 to 1.0
 
 // based on Charles' code
 const revealAnswer = () => {
